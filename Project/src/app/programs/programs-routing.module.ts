2fbbe1890b66aaa41dd28fccba99437eef63e498
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-03-20 14:06:07 +0200

    Added Routing

--- Project/src/app/programs/programs-routing.module.ts
+++ Project/src/app/programs/programs-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'edit', component: EditComponent },
+  { path: 'catalog', component: CatalogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProgramsRoutingModule { }
+export class ProgramsRoutingModule {}
