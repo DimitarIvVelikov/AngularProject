d8fe9b9a11bb0c799d47726a355ba6c430982ac1
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-03-20 12:39:23 +0200

    Added Core Module

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,25 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { CatalogComponent } from './programs/catalog/catalog.component';
-import { CreateComponent } from './programs/create/create.component';
-import { EditComponent } from './programs/edit/edit.component';
 import { UserModule } from './user/user.module';
+import { ProgramsModule } from './programs/programs.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    CatalogComponent,
-    CreateComponent,
-    EditComponent,
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserModule,
+    ProgramsModule,
+    CoreModule,
   ],
-  imports: [BrowserModule, AppRoutingModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
