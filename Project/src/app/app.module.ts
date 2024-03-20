049f6736623e80da88c82818fa36f9ee8e7ecf79
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-03-20 12:26:16 +0200

    Modifications from component generation

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { CatalogComponent } from './programs/catalog/catalog.component';
+import { CreateComponent } from './programs/create/create.component';
+import { EditComponent } from './programs/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    CatalogComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
