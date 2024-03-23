1c67b2f5423b0fd07bcc5574cec2aa9d11d6c61f
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-03-23 23:08:32 +0200

    Added Interceptor and HttpModule

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +9,7 @@ import { TrainingProgramsModule } from './training-programs/training-programs.mo
 import { CoreModule } from './core/core.module';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
+import { appInterceptorProvider } from './app.interceptor';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -20,8 +21,9 @@ import { SharedModule } from './shared/shared.module';
     CoreModule,
     RouterModule,
     SharedModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [appInterceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
