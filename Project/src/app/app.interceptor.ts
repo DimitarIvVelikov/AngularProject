2104798043834d96f7559fd2d6508e87f0a03962
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-03-23 23:07:58 +0200

    Added API Interceptor

---
+++ Project/src/app/app.interceptor.ts
@@ -0,0 +1,45 @@
+import { Injectable, Provider } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
+import { environment } from 'src/environments/environments';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AppInterceptor implements HttpInterceptor {
+  API = '/api';
+  constructor(private router: Router) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (request.url.startsWith(this.API)) {
+      request = request.clone({
+        url: request.url.replace(this.API, environment.baseURL),
+        withCredentials: true,
+      });
+    }
+    console.log(request);
+
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (error.status === 400) {
+          this.router.navigate(['/login']);
+        }
+        return [error];
+      })
+    );
+  }
+}
+
+export const appInterceptorProvider: Provider = {
+  useClass: AppInterceptor,
+  multi: true,
+  provide: HTTP_INTERCEPTORS,
+};
