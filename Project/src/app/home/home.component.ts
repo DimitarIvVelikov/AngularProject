ddff4379cdc9921c31642e8687dec7b4f904f583
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-03-26 20:19:39 +0200

    Added Dynamic Rendering of Training Programs

--- Project/src/app/home/home.component.ts
+++ Project/src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrainingProgramsService } from '../training-programs/training-programs.service';
+import { TrainingPrograms } from '../types/trainingPrograms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  trainingPrograms: TrainingPrograms[] = [];
 
+  constructor(private trainingProgramService: TrainingProgramsService) {}
+  ngOnInit(): void {
+    this.trainingProgramService
+      .getAllTrainingPrograms()
+      .subscribe((trainingPrograms) => {
+        this.trainingPrograms = trainingPrograms;
+        console.log(this.trainingPrograms);
+      });
+  }
 }
