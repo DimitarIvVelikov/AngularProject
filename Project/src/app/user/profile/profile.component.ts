664e2b149bcb792e8503e3b797b2307963d290d8
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-03-26 23:31:03 +0200

    Added Functional Part of Profile

--- Project/src/app/user/profile/profile.component.ts
+++ Project/src/app/user/profile/profile.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { UserForAuth } from 'src/app/types/user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
-
+export class ProfileComponent implements OnInit {
+  profile: UserForAuth | undefined;
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.userService.getProfile().subscribe((profile) => {
+      console.log(profile);
+      this.profile = profile;
+    });
+  }
 }
