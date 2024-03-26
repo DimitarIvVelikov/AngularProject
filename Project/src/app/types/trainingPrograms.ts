570f353422d66b4f58cda5472244843d495c673c
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-03-26 20:14:52 +0200

    Added Training Programs Type

---
+++ Project/src/app/types/trainingPrograms.ts
@@ -0,0 +1,11 @@
+export interface TrainingPrograms {
+  _id: string;
+  name: string;
+  description: string;
+  duration: number;
+  difficulty: string;
+  created_at: string;
+  updatedAt: string;
+  owner: string;
+  signUpList: string[];
+}
