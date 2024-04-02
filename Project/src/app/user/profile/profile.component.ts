c161510df902eadcf9d3b00af6a55d768f1e5072
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-04-02 17:21:16 +0300

    Main Implementation of Profile Page

--- Project/src/app/user/profile/profile.component.ts
+++ Project/src/app/user/profile/profile.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../user.service';
 import { UserForAuth } from 'src/app/types/user';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environments';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
 
 @Component({
   selector: 'app-profile',
@@ -8,12 +11,49 @@ import { UserForAuth } from 'src/app/types/user';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  profile: UserForAuth | undefined;
-  constructor(private userService: UserService) {}
+  profile: UserForAuth = {
+    _id: '',
+    username: '',
+    email: '',
+    created_at: '',
+    updatedAt: '',
+  };
+  editView: boolean = false;
+  domains: string[] = environment.domains;
+
+  form = this.fb.group({
+    username: [this.profile?.username, [Validators.required]],
+    email: [
+      this.profile?.email,
+      [Validators.required, emailValidator(this.domains)],
+    ],
+  });
+  constructor(private userService: UserService, private fb: FormBuilder) {}
   ngOnInit(): void {
     this.userService.getProfile().subscribe((profile) => {
-      console.log(profile);
       this.profile = profile;
+      this.form.setValue({
+        username: this.profile?.username,
+        email: this.profile?.email,
+      });
+    });
+  }
+
+  toggleEdit() {
+    this.editView = !this.editView;
+  }
+
+  editProfile() {
+    if (this.form.invalid) return;
+    const { username, email } = this.form.value;
+    this.profile = {
+      ...this.profile,
+      username: username,
+      email: email,
+    } as UserForAuth;
+
+    this.userService.updateProfile(this.profile).subscribe((profile) => {
+      location.reload();
     });
   }
 }
