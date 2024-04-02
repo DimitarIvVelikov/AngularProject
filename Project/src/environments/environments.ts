da3f852d5b2f597048731e0ab3f19993142c220b
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-04-02 14:55:14 +0300

    Adding URL Validator Expression

--- Project/src/environments/environments.ts
+++ Project/src/environments/environments.ts
@@ -1,4 +1,6 @@
+// prettier-ignore
 export const environment = {
   domains: [`gmail\.com`, `abv\.bg`, `yahoo\.com`],
   baseURL: 'http://127.0.0.1:5000/api',
+  imageUrlValidator: '^https?:\/\/',
 };
