14c6473a8018d0b9f79da38f1aa401bd173897af
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-04-02 19:24:24 +0300

    Modified Program Type to have proper interfaces

--- Project/src/app/types/user.ts
+++ Project/src/app/types/user.ts
@@ -1,7 +1,11 @@
+import { TrainingPrograms } from './trainingPrograms';
+
 export interface UserForAuth {
   _id: string;
   username: string;
   email: string;
   created_at: string;
   updatedAt: string;
+  signUpList: TrainingPrograms[];
+  createdList: TrainingPrograms[];
 }
