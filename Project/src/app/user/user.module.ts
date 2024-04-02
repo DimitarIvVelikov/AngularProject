b73dc09e8459f8aa8d77fe53f0241c2a01460401
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-04-02 17:20:09 +0300

    Added Dynamic Forms

--- Project/src/app/user/user.module.ts
+++ Project/src/app/user/user.module.ts
@@ -5,11 +5,17 @@ import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ProfileComponent],
-  imports: [CommonModule, UserRoutingModule, FormsModule, SharedModule],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    FormsModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class UserModule {}
