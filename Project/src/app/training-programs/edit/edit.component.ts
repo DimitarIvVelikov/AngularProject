65aabab90028d331f05291dd97dbdf2245d4a2a0
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-04-02 17:25:19 +0300

    Added Edit Page
    Main Implementation of edit page

--- Project/src/app/training-programs/edit/edit.component.ts
+++ Project/src/app/training-programs/edit/edit.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TrainingProgramsService } from '../training-programs.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TrainingPrograms } from 'src/app/types/trainingPrograms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
+  trainingProgram: TrainingPrograms = {
+    _id: '',
+    name: '',
+    description: '',
+    duration: 0,
+    difficulty: '',
+    created_at: '',
+    updatedAt: '',
+    owner: '',
+    signUpList: [],
+    imageUrl: '',
+  };
+  constructor(
+    private trainingProgramsService: TrainingProgramsService,
+    private router: Router,
+    private activeRoute: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    const programId = this.activeRoute.params.subscribe((params) => {
+      const programId = params['trainingId'];
+      this.trainingProgramsService
+        .getTrainingProgram(programId)
+        .subscribe((program) => {
+          this.trainingProgram = program;
+        });
+    });
+  }
+  edit(form: NgForm) {
+    if (form.invalid) return;
+
+    const { name, description, duration, difficulty } = form.value;
+
+    this.trainingProgramsService
+      .editTrainingProgram(this.trainingProgram._id, {
+        name,
+        description,
+        difficulty,
+        duration,
+      })
+      .subscribe((program) => {
+        this.router.navigate(['/catalog/' + this.trainingProgram._id]);
+      });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/catalog/' + this.trainingProgram._id]);
+  }
 }
