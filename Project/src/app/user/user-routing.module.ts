e3a9bdb76335c8a051bcb5394ad05ac2859cf0cd
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-04-03 18:58:42 +0300

    Implementation of a Route Guard

--- Project/src/app/user/user-routing.module.ts
+++ Project/src/app/user/user-routing.module.ts
@@ -3,11 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthActivate } from '../guards/auth.guard';
+// import { AuthDeactivate } from '../guards/deactivate-auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    // canActivate: [AuthDeactivate],
+  },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthActivate] },
 ];
 
 @NgModule({
