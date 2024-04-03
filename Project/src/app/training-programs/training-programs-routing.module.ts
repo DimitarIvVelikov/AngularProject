e2d13075d5fc79bf7785b9b0eda778fa94be4044
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-04-03 19:00:40 +0300

    Added Route Guard

--- Project/src/app/training-programs/training-programs-routing.module.ts
+++ Project/src/app/training-programs/training-programs-routing.module.ts
@@ -4,13 +4,18 @@ import { CreateComponent } from './create/create.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { EditComponent } from './edit/edit.component';
 import { CurrentTrainingProgramComponent } from './current-training-program/current-training-program.component';
+import { TrainingOwnerGuard } from '../guards/trainingProgram.guard';
+import { AuthActivate } from '../guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'create', component: CreateComponent },
-  { path: 'edit', component: EditComponent },
+  { path: 'create', component: CreateComponent, canActivate: [AuthActivate] },
   { path: 'catalog', component: CatalogComponent },
   { path: 'catalog/:trainingId', component: CurrentTrainingProgramComponent },
-  { path: 'catalog/:trainingId/edit', component: EditComponent },
+  {
+    path: 'catalog/:trainingId/edit',
+    component: EditComponent,
+    canActivate: [TrainingOwnerGuard],
+  },
 ];
 
 @NgModule({
