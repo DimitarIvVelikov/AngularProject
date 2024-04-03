75904f5de7d6f006dd5e808ceef2ef16ff6a7681
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-04-03 22:49:42 +0300

    Test implementaion

--- Project/src/app/guards/deactivate-auth.guard.ts
+++ Project/src/app/guards/deactivate-auth.guard.ts
@@ -8,6 +8,7 @@
 // } from '@angular/router';
 // import { Observable } from 'rxjs';
 // import { UserService } from '../user/user.service';
+// import { canActivate } from '../shared/utils/canActivate';
 
 // @Injectable({ providedIn: 'root' })
 // export class AuthDeactivate implements CanActivate {
@@ -20,6 +21,6 @@
 //     | UrlTree
 //     | Observable<boolean | UrlTree>
 //     | Promise<boolean | UrlTree> {
-//     return !this.userService.isLoggedIn;
+//     return canActivate(route, state);
 //   }
 // }
